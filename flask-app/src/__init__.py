70dd86a9653b4a6c306bbcddc15333e9ac9df8ba
“airman416” <im.armaan.agrawal@gmail.com>
2023-12-05 14:43:23 -0500

    Added blueprints

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -33,15 +33,19 @@ def create_app():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
     # Import the various Beluprint Objects
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.users.users import user
+    from src.coach.coach import coach
+    from src.athlete.athlete import athlete
     from src.admin.admin import admin
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     # app.register_blueprint(customers,   url_prefix='/c')
     # app.register_blueprint(products,    url_prefix='/p')
-    app.register_blueprint(admin,    url_prefix='/a')
+    app.register_blueprint(admin,    url_prefix='/admin')
+    app.register_blueprint(user,    url_prefix='/user')
+    app.register_blueprint(coach,    url_prefix='/coach')
+    app.register_blueprint(athlete,    url_prefix='/athlete')
 
     # Don't forget to return the app object
     return app
